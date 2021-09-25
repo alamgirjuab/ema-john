8440bd582d3c6eabdc9efa0b8715d891d4cdcbf0
alamgirjuab <alamgirjuab@gmail.com>
2021-09-25 23:16:35 +0600

    assignment-8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './component/Header/Header';
-import Shop from './component/Shop/Shop';
-
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+
     </div>
   );
 }
